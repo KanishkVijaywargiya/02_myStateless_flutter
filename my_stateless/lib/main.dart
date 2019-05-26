2c3e32dfea413c622e82c894778a63a99f6fddca
Kanishk <kanishkvijaywargiya99@gmail.com>
2019-05-26 12:49:41 +0530

    third commit

---
+++ my_stateless/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: 'Flutter App',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.green,
+      accentColor: Colors.red,
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('I am text line one'),
+            Text('I am text line two'),
+            RaisedButton(
+              onPressed: (){},
+              child: Text('Signup'),
+              color: Colors.redAccent,
+              splashColor: Colors.white,
+
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(
+          Icons.add_a_photo,
+          color: Colors.white,
+        ),
+      ),
+    ),
+  );
+  }
+}
+
+
